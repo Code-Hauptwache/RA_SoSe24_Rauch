03c124e7c7bb43fcd19a03c825d3ef06c4e16caa
zaka <zb2003boujana@gmail.com>
2024-05-27 15:40:15 +0200

    wrksht4 tsk1 and 2

---
+++ zaka/worksheet04/task2.asm
@@ -0,0 +1,11 @@
+	addi	$8,$8,0
+	addi	$9,$9,11
+	addi	$10,$10,0x1000
+	addi	$11,$11,-1
+	addi	$12,$12,-0x8000
+	ori	$13,$13,0x8000
+	lui	$14,0xffff
+	lui	$15,0x7fff
+	ori	$15,$15,0xffff
+	addi	$24,$24,5322
+	addi	$25,$25,75
