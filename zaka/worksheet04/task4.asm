2e4b1629f83c73e5722f0095339eafaf153bc48e
zaka <zb2003boujana@gmail.com>
2024-05-28 01:01:36 +0200

    Create task4.asm

---
+++ zaka/worksheet04/task4.asm
@@ -0,0 +1,5 @@
+.data
+num1:	.byte 	0xa0		# Single byte value: 0xa0
+num2:	.half	0xb1b0		# Two-byte (short) value: 0xb1b0
+num3:	.word	0xc3c2c1c0	# Four-byte (long) value: 0xc3c2c1c0
+str1:	.asciiz "Das Ende"	# Null-terminated ASCII string: "Das Ende"
