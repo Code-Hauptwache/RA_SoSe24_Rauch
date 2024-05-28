c67572f875fe09e9b158d5cadc912b5bd2a4346f
Yassin561 <yassin.elamraoui@hotmail.com>
2024-05-28 13:19:21 +0200

    RA worksheet03

---
+++ yassin/worksheet04/task2.asm
@@ -0,0 +1,11 @@
+	add 	$8,$0,$0
+	addi 	$9,$0,11
+	addi	$10,$0,0x1000
+	addi	$11,$0,-1
+	addi	$12,$0,-0x8000
+	ori 	$13,$0,0x8000
+	lui	$14,0xffff
+	lui	$15,0x7fff
+	ori	$15,$15,0xffff
+	addi	$24,$0,5322
+	addi	$25,$0,75
---
+++ yassin/worksheet04/task4.asm
@@ -0,0 +1,5 @@
+.data
+n1:	.byte	0xa0
+n2:	.half 	0xb1b0
+n3:	.word 	0xc3c2c1c0
+w1:	.asciiz "Das Ende"
