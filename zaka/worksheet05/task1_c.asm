0cf72b2e972ed76e049e5e99eaca4f81ffa5dec3
zaka <zb2003boujana@gmail.com>
2024-06-02 21:24:02 +0200

    Complet worksheet05

---
+++ zaka/worksheet05/task1_c.asm
@@ -0,0 +1,32 @@
+main:
+    # Initialize arguments
+    ori     $4, $0, 210       	# Initialize r4 to 17
+    ori     $5, $0, 28		# Initialize r5 to 51
+
+    # Call the subroutine
+    jal     getGCD           	# Jump and link to subroutine "getGCD"
+
+    # End of program
+end:
+    j       end              	# Infinite loop to end the program
+
+
+# Subroutine to calculate GCD
+getGCD:    
+    beq     $4, $0, end      	# If r4 == 0, go to end (loop)
+    beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD
+
+calcGCD:
+    beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD
+    slt     $1, $5, $4       	# Set $1 to 1 if r5 < r4
+    beq     $1, $0, sub_r5_r4 	# If r5 >= r4, go to sub_r5_r4
+    sub     $4, $4, $5       	# r4 = r4 - r5
+    j       calcGCD          	# Jump back to calcGCD
+
+sub_r5_r4:
+    sub     $5, $5, $4       	# r5 = r5 - r4
+    j       calcGCD          	# Jump back to calcGCD
+
+storeGCD:
+    addu    $2, $0, $4       	# Store the GCD in r2
+    jr      $ra              	# Return to caller
