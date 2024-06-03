3e4060f29f320187001c49b874b4ed4b7ff4624b
zaka <zb2003boujana@gmail.com>
2024-06-03 23:11:01 +0200

    Modify task 1 c) and d)

--- zaka/worksheet05/task1_c.asm
+++ zaka/worksheet05/task1_c.asm
@@ -1,10 +1,10 @@
 main:
     # Initialize arguments
-    ori     $4, $0, 30       	# Initialize r4 to 17
-    ori     $5, $0, 25		# Initialize r5 to 51
+    ori     $4, $0, 0       	# Initialize r4 to 17
+    ori     $5, $0, 0			# Initialize r5 to 51
 
     # Call the subroutine
-    jal     getGCD           	# Jump and link to subroutine "getGCD"
+	jal     getGCD           	# Jump and link to subroutine "getGCD"
 
     # End of program
 end:
@@ -13,9 +13,8 @@ end:
 
 # Subroutine to calculate GCD
 getGCD:
-    or	    $2, $0, $5    	# Initialize r2 with the value in r5
-    beq     $4, $0, end      	# If r4 == 0, go to end (loop)
-    beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD
+    beq     $4, $0, storeGCD 	# If r4 == 0, go to storeGCD (store r5 as the GCD)
+    beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD (store r4 as the GCD)
 
 calcGCD:
     beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD
@@ -29,5 +28,7 @@ sub_r5_r4:
     j       calcGCD          	# Jump back to calcGCD
 
 storeGCD:
-    addu    $2, $0, $4       	# Store the GCD in r2
+    # Store the non-zero value of r4 or r5 as the GCD
+    or      $1, $4, $5       	# $1 = r4 | r5
+    addu    $2, $0, $1       	# Store the GCD in r2
     jr      $ra              	# Return to caller
--- zaka/worksheet05/task1_d.asm
+++ zaka/worksheet05/task1_d.asm
@@ -1,39 +1,43 @@
 main:
     # Initialize arguments and Call the subroutine
     ori     $4, $0, 25       	# Initialize r4 to 17
-    ori     $5, $0, 30		# Initialize r5 to 51
+    ori     $5, $0, 30			# Initialize r5 to 51
     jal     getGCD           	# Jump and link to subroutine "getGCD"
 
     # Initialize arguments and Call the subroutine
     ori     $4, $0, 210       	# Initialize r4 to 17
-    ori     $5, $0, 28		# Initialize r5 to 51
+    ori     $5, $0, 28			# Initialize r5 to 51
     jal     getGCD           	# Jump and link to subroutine "getGCD"
 
     # Initialize arguments and Call the subroutine
     ori     $4, $0, 49       	# Initialize r4 to 17
-    ori     $5, $0, 42		# Initialize r5 to 51
+    ori     $5, $0, 42			# Initialize r5 to 51
     jal     getGCD           	# Jump and link to subroutine "getGCD"
 
     # Initialize arguments and Call the subroutine
     ori     $4, $0, 17       	# Initialize r4 to 17
-    ori     $5, $0, 3		# Initialize r5 to 51
+    ori     $5, $0, 3			# Initialize r5 to 51
     jal     getGCD           	# Jump and link to subroutine "getGCD"
 
     # Initialize arguments and Call the subroutine
     ori     $4, $0, 17       	# Initialize r4 to 17
-    ori     $5, $0, 51		# Initialize r5 to 51
+    ori     $5, $0, 51			# Initialize r5 to 51
     jal     getGCD           	# Jump and link to subroutine "getGCD"
-
+    
+    # Initialize arguments and Call the subroutine (both zero case)
+    ori     $4, $0, 0       	# Initialize r4 to 0
+    ori     $5, $0, 0			# Initialize r5 to 0
+    jal     getGCD           	# Jump and link to subroutine "getGCD"
+    
     # End of program
 end:
     j       end              	# Infinite loop to end the program
 
 
 # Subroutine to calculate GCD
-getGCD:    
-    or	    $2, $0, $5    	# Initialize r2 with the value in r5
-    beq     $4, $0, end      	# If r4 == 0, go to end (loop)
-    beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD
+getGCD:
+    beq     $4, $0, storeGCD 	# If r4 == 0, go to storeGCD (store r5 as the GCD)
+    beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD (store r4 as the GCD)
 
 calcGCD:
     beq     $5, $0, storeGCD 	# If r5 == 0, go to storeGCD
@@ -47,5 +51,7 @@ sub_r5_r4:
     j       calcGCD          	# Jump back to calcGCD
 
 storeGCD:
-    addu    $2, $0, $4       	# Store the GCD in r2
+    # Store the non-zero value of r4 or r5 as the GCD
+    or      $1, $4, $5       	# $1 = r4 | r5 (will be non-zero)
+    addu    $2, $0, $1       	# Store the GCD in r2
     jr      $ra              	# Return to caller
