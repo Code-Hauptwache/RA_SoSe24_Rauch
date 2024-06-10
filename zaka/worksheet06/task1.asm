fd614b3c84a2128b9b08e01cc4d67bc0f6ba9187
zakaBouj <zb2003boujana@gmail.com>
2024-06-10 17:31:37 +0200

    update tsk6.1a)

--- zaka/worksheet06/task1.asm
+++ zaka/worksheet06/task1.asm
@@ -3,9 +3,9 @@ str:	.asciiz	"TEST String!"
 
 main:
 	# Call subroutine	
-	
-	
+	la	$a0, str			# load adress of str
+	jal	strToLower			# Jump and link to subroutine "strToLower"
 	
 	# Subroutine to turn letters to lower case
 strToLower:
-	
+	slt	
