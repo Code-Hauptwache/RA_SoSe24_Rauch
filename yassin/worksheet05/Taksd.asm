0a212327e9796199335d023582b1a02146f589f7
Yassin561 <yassin.elamraoui@hotmail.com>
2024-06-17 16:47:24 +0200

    recovery worksheet05

--- yassin/worksheet05/Taksd.asm
+++ yassin/worksheet05/Taksd.asm
@@ -26,14 +26,22 @@ end_main:
     	
 gcd:
     	add	$2, $5,$0   		# r2 <-- r5
-loop_sub:
+
     	beq	$4, $0, end_sub 		# if(r4 == 0) goto end_sub
-    	beq	$5, $0, set_sub_r2 		# if(r5 == 0) goto set_sub_r2
-   	beq	$5, $4, sub_sub_r4  	# if(r4 > r5) goto sub_sub_r4
+loop_sub:
+       	beq	$5, $0, set_sub_r2 		# if(r5 == 0) goto set_sub_r2
+   	
+   	# if(r5 > r4) goto sub_r5_r4
+    	slt	$6, $5, $4
+    	beq	$6, $0, sub_r5_r4
+    	# else goto sub_r4_r5
+    	j	sub_r4_r5
+
+sub_r5_r4:
     	sub	$5, $5, $4        		# r5 <-- r5 - r4
     	j 	loop_sub               	# goto loop_sub
 
-sub_sub_r4:
+sub_r4_r5:
     	sub	$4, $4, $5        		# r4 <-- r4 - r5
     	j	loop_sub               	# goto loop_sub
 
