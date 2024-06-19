0c53c86c6777aeefd221c7cf00e5ec35a39e24ce
zaka <zb2003boujana@gmail.com>
2024-06-20 01:10:30 +0200

    6.1 b)

--- zaka/worksheet06/task1.asm
+++ zaka/worksheet06/task1.asm
@@ -27,9 +27,9 @@ strToLower:
 	
 # Loop to turn every capital letter in a string to lowercase
 loop:
-    	lb 	$t3, ($a0)  				# Load the current byte (character) of str into $t3
+    	lb 	$t3, ($a0)  				# Load the current byte (character) of string into $t3
 	
-	beq	$t3, $zero, end				# If $t3 == '\0' jump to end
+	beq	$t3, $zero, strToLower_end		# If $t3 == '\0' jump to strToLower_end
 
 	slt	$t4, $t3, $t0 				# If $t3 < 'A' set $t4 to 1
     	slt	$t5, $t1, $t3 				# If $t3 > 'Z' set $t5 to 1
@@ -46,9 +46,69 @@ nextChar:
 	addi	$a0, $a0, 1				# Increment adress of str to point to the next char in string
 	j	loop	
 
-end:
+strToLower_end:
 	jr	$ra					# Return from subroutine
 
-# Subroutine strTurnAround(cahr*)
+# Subroutine strTurnAround(char*)
+
 strTurnAround:
-	
+
+    move    $t3, $a0           # $t3 = address of the string (start_ptr)
+
+    li      $t0, 0             # $t0 = string length counter (initially 0)
+
+
+
+# Loop to get the length of the given string
+
+getStrLen:
+
+    lb      $t4, 0($t3)        # Load byte from address $t3 into $t4
+
+    beq     $t4, $zero, turnAround # If current char == '\0' goto turnAround
+
+    addi    $t0, $t0, 1        # Increment string length count
+
+    addi    $t3, $t3, 1        # Move to the next character
+
+    j       getStrLen          # Jump to getStrLen
+
+
+
+turnAround:
+
+    subu    $t1, $t3, 1        # Set $t1 to point at the last character ($t3 points at '\0')
+
+    move    $t2, $a0           # $t2 = start_ptr for swapping
+
+    
+
+# Loop to swap characters
+
+swapLoop:
+
+    bge     $t2, $t1, strTurnAround_end # If start_ptr >= end_ptr, end the loop
+
+    
+
+    lb      $t4, 0($t2)        # Load byte from start_ptr into $t4
+
+    lb      $t5, 0($t1)        # Load byte from end_ptr into $t5
+
+    sb      $t5, 0($t2)        # Store byte from $t5 into start_ptr
+
+    sb      $t4, 0($t1)        # Store byte from $t4 into end_ptr
+
+    
+
+    addi    $t2, $t2, 1        # Increment start_ptr
+
+    subi    $t1, $t1, 1        # Decrement end_ptr
+
+    j       swapLoop           # Jump to swapLoop
+
+
+
+strTurnAround_end:
+
+    jr      $ra                # Return from subroutine
