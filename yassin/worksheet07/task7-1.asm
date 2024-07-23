acc9094e0f95b4c6cb486a31c83afef1b551638b
Yassin561 <yassin.elamraoui@hotmail.com>
2024-07-23 16:44:45 +0200

    Update 7,1 task

--- yassin/worksheet07/task7-1.asm
+++ yassin/worksheet07/task7-1.asm
@@ -7,14 +7,21 @@ some_string:
 
 main:
 	la	$a0,some_string
+	li	$t3, 0		#zähler für c
 	jal	ncstr	
 
-#subroutine int ncstr(char *str, char c)
-ncstr:
-	li	$t1, 'c'
+	#subroutine int ncstr(char *str, char c)
+loop:
 	lb	$t2,($a0)
+	li	$t1, 'c'
+	beq	$t2, $t3, 
 	beq	$t2, $zero, end
 	beq 	$t2, $t1, anzahlc	# c anzahl um 1 erhöhen	
+	
+
+next:
+	addi 	$a0, $a0, 1
+	j	ncstr
 	
 anzahlc:
 	
